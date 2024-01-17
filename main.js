a119e1a11a8ae1335b0eb3f9fdca462c96241de8
AnnaNowak96 <jankowska.anna-96@wp.pl.noreply.github.com>
2024-01-17 15:36:28 +0100

    find a operator char, find first and second number, add back

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-//klikam na cyfre i ona sie pojawia w oknie
-//clear = restart ekranu
+let firstNumber = {};
+let secondNumber = {};
+let operatorPoint = {};
+let operatorPointPlace = {};
+
 
 function displayClicks() {
 const buttons = document.querySelectorAll(".btn");
@@ -7,15 +10,104 @@ const textAera = document.querySelector("#window");
 
 buttons.forEach(btn => {
     btn.addEventListener("click", () => {
-        textAera.innerText += btn.innerText;
+        textAera.innerText = textAera.innerText +btn.innerText;
+        let text = textAera.innerText;
+        console.log(text);
+        console.log(typeof(text));
     });
 });
 }
 
-displayClicks();
+function back() {
+    const textAera = document.querySelector("#window");
+    let text = textAera.innerText;
+    let result = text.slice(0, -1);
+    textAera.innerText = result;
+}
+
+
+function findTheNumbers() {
+    const textAera = document.querySelector("#window");
+    let text = textAera.innerText;
+    let chars = ["+", "-", "/", "*"];
+    let operatorPoint = [];
+
+    chars.forEach(char => {
+        if (text.includes(char)) {
+            operatorPoint.push(char);
+        }
+    });
+
+    let operatorPointPlace = text.indexOf(operatorPoint);
+    console.log(operatorPoint);
+    console.log(operatorPointPlace);
+    let firstNumber = text.slice(0, operatorPointPlace);
+    console.log(firstNumber);
+    let secondNumber = text.slice(operatorPointPlace+1);
+    console.log(secondNumber);
+}
+
+// function test() {
+//     const textAera = document.querySelector("#window");
+//     let text = textAera.innerText;
+//     let chars = ["+", "-", "/", "*"];
+//     let usedChar = [];
+
+//     chars.forEach(char => {
+//         if (text.includes(char)) {
+//             usedChar.push(char);
+//         }
+//     });
+
+//     console.log(usedChar);
+// }
+
+
+
+function operate(operatorPoint) {
+findTheNumbers();
+    if (operatorPoint === "+") {
+        adding();
+    } else if (operatorPoint === "-") {
+        minus();
+    } else if (operatorPoint === "*") {
+        multiplay();
+    } else if (operatorPoint === "/") {
+        divide();
+    }
+}
+
+// Math functions
+function adding(firstNumber, secondNumber) {
+    let sum = firstNumber + secondNumber;
+    console.log(sum);
+}
+
+function minus(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function multiplay(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+    if (firstNumber / 0) {
+        alert("Don't divide by 0!!!");
+    } else {
+        return firstNumber / secondNumber;
+    }
+}
+//
+
+
 
 function clear () {
     location.reload();
 }
 
+
+displayClicks();
 document.getElementById("clear").addEventListener("click", clear);
+document.getElementById("back").addEventListener("click", back);
+document.getElementById("enter").addEventListener("click", operate);
